6f1e2cab43bbd77bd4bab17e6f376b1652863a29
sadeqfwade <sadeq202077@gmail.com>
2024-03-14 00:47:04 +0300

    sadeq

--- lib/loginscreen/loginScreen.dart
+++ lib/loginscreen/loginScreen.dart
@@ -21,7 +21,7 @@ class _loginScreenState extends State<loginScreen> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        backgroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 230, 33, 33),
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 50,right: 20,left: 20,),
